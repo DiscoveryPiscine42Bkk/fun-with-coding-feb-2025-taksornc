a3340c191213b179d4fc24de11db4201c73043b4
tttkk123 <66050172@kmitl.ac.th>
2025-02-06 04:23:46 +0700

    Update todo.js

--- discovery_piscine_BTC/cell03/ex03/todo.js
+++ discovery_piscine_BTC/cell03/ex03/todo.js
@@ -1,46 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
-    loadTodos();
-
-     document.getElementById('new').addEventListener('click', () => {
-    const textxas = prompt('Enter a new TO DO:');
-    if (textxas) {
-      textxas.push(textxas); 
-      addTaskToDOM(textxas);
-      saveTasks(textxas); 
-        }
-    });
-});
-
-function addTodo(textxas) {
-    let todo = document.createElement("div");
-    todo.className = "todo";
-    todo.textContent = textxas;
-    todo.addEventListener("click", function () {
-        if (confirm("Do you want to delete this TODO?")) {
-            todo.remove();
-            saveTodos();
-        }
-    });
-    let list = document.getElementById("ft_list");
-    list.prepend(todo);
+function saveToStorage(data) {
+    localStorage.setItem('todoData', JSON.stringify(data));
 }
 
-function saveTodos() {
-    let todos = [];
-    document.querySelectorAll(".todo").forEach(todo => {
-        todos.push(todo.textContent);
-    });
-    document.cookie = "todos=" + JSON.stringify(todos) + "; path=/";
+function getFromStorage() {
+    const data = localStorage.getItem('todoData');
+    return data ? JSON.parse(data) : [];
 }
 
-function loadTodos() {
-    let cookies = document.cookie.split("; ");
-    for (let cookie of cookies) {
-        let [name, value] = cookie.split("=");
-        if (name === "todos") {
-            let todos = JSON.parse(value);
-            todos.forEach(todo => addTodo(todo));
-        }
+function startTodoApp() {
+    let todoItems = getFromStorage();
+    const todoList = document.getElementById('ft_list');
+
+    function renderTodoItem(content) {
+        const element = document.createElement('div');
+        element.textContent = content;
+
+        element.onclick = function() {
+            if (!confirm('Do you want to remove this TODO?')) {
+                return;
+            }
+            const pos = todoItems.indexOf(content);
+            if (pos >= 0) {
+                todoItems.splice(pos, 1);
+                element.remove();
+                saveToStorage(todoItems);
+            }
+        };
+
+        todoList.insertBefore(element, todoList.firstChild);
     }
+
+    todoItems.forEach(renderTodoItem);
+
+    document.getElementById('new').onclick = function() {
+        const content = prompt('Enter a new TODO:');
+        if (!content) {
+            return;
+        }
+        todoItems.unshift(content);
+        renderTodoItem(content);
+        saveToStorage(todoItems);
+    };
 }
 
+window.onload = startTodoApp;
